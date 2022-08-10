0419ca48b59a39e543c7c7105e60d3eff4bd20f2
Adam Stox <adamstox@protonmail.com>
2022-08-10 16:57:45 -0300

    Add mint and burn functions

--- contract/src/TimeCapsule.sol
+++ contract/src/TimeCapsule.sol
@@ -2,9 +2,24 @@
 pragma solidity ^0.8.13;
 
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract TimeCapsule is Ownable {
+contract TimeCapsule is Ownable, ERC721Enumerable {
 
-    constructor(){
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) public {
+    }
+
+    mapping(uint => address) ownership;
+    mapping(uint => string) messages;
+
+    function mint(address to, string calldata message) public payable {
+        uint256 id = totalSupply();
+        messages[id] = message;
+
+        _safeMint(to, id);
+    }
+
+    function burn(uint256 id) onlyOwner public {
+        _burn(id);
     }
 }
--- contract/test/TimeCapsule.t.sol
+++ contract/test/TimeCapsule.t.sol
@@ -5,10 +5,57 @@ import {TimeCapsule} from "src/TimeCapsule.sol";
 import "forge-std/Test.sol";
 
 contract TimeCapsuleTest is Test {
-    TimeCapsule timeCap = new TimeCapsule();
+    TimeCapsule timeCap = new TimeCapsule("TimeCapsule", "TIME");
     function setUp() public {}
 
-    function testExample() public {
+    function testOwner() public {
         assertEq(address(timeCap.owner()), address(this));
+    }
+
+    function testMintSuccess(address addr) public {
+        vm.assume(addr != address(0));
+        vm.assume(addr.code.length <= 0);
+        timeCap.mint(addr, "hello world");
+        assertEq(timeCap.ownerOf(0), addr);
+    }
+
+    function testMintTwiceSuccess(address addr) public {
+        vm.assume(addr != address(0));
+        vm.assume(addr.code.length <= 0);
+        timeCap.mint(addr, "hello world");
+        timeCap.mint(addr, "ok ok ok ok ok");
+        assertEq(timeCap.ownerOf(0), addr);
+        assertEq(timeCap.ownerOf(1), addr);
+    }
+
+
+    function testFailMintToZero() public {
+        timeCap.mint(address(0), "hello world");
+        assertEq(timeCap.ownerOf(0), address(0));
+    }
+
+    function testFailMintToContract() public {
+        timeCap.mint(address(this), "hello world");
+        assertEq(timeCap.ownerOf(0), address(this));
+    }
+
+    function testBurn(address addr) public {
+        vm.assume(addr != address(0));
+        vm.assume(addr.code.length <= 0);
+        timeCap.mint(addr, "hello world");
+        assertEq(timeCap.ownerOf(0), addr);
+        timeCap.burn(0);
+        vm.expectRevert("ERC721: invalid token ID");
+        timeCap.ownerOf(0);
+    }
+
+    function testBurnUnauthorized(address addr1, address addr2) public {
+        vm.assume(addr1 != address(0));
+        vm.assume(addr1.code.length <= 0);
+        timeCap.mint(addr1, "hello world");
+        assertEq(timeCap.ownerOf(0), addr1);
+        vm.prank(addr2);
+        vm.expectRevert("Ownable: caller is not the owner");
+        timeCap.burn(0);
     }
 }
