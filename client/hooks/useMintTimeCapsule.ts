e5f7a00bc69f6775f26a0a455926233946e54aa3
JP Angelle <jp@centrifuge.io>
2022-08-09 21:33:12 -0500

    fix: type

--- client/hooks/useMintTimeCapsule.ts
+++ client/hooks/useMintTimeCapsule.ts
@@ -1,5 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
+import { Inputs } from '../types';
 
 const useMintTimeCapsule = () => {
   const query = useMutation(async (payload: Inputs) => {
