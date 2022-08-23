ebe14e85698eb61cce6db4df0568d0e2ee793191
JP Angelle <jp@centrifuge.io>
2022-08-23 13:18:11 -0500

    feat: add marketplace endpoint

---
+++ client/pages/api/getTimeCapsuleForMarketplace/[id].ts
@@ -0,0 +1,51 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import getTimeCapsuleFromBucket from '../../../utilities/db/getTimeCapsuleFromBucket';
+import queryTimeCapsule from '../../../utilities/queries/queryTimeCapsule';
+
+type Response =
+  | {
+      name: 'Centrifuge Time Capsule';
+      description: 'Your prediction for DeFi in 2023';
+      image: string;
+    }
+  | string
+  | Error;
+
+interface Request extends NextApiRequest {
+  query: {
+    id: string;
+  };
+}
+
+export default async function handler(
+  req: Request,
+  res: NextApiResponse<Response>,
+) {
+  const { query, method } = req;
+
+  if (method !== 'GET') {
+    res.status(405).send('Method not allowed. Use POST.');
+  }
+
+  try {
+    const { nft } = await queryTimeCapsule(query.id);
+
+    const svgLink = await getTimeCapsuleFromBucket(query.id);
+
+    if (nft && svgLink) {
+      res.status(200).json({
+        name: 'Centrifuge Time Capsule',
+        description: 'Your prediction for DeFi in 2023',
+        image: svgLink,
+      });
+    } else {
+      res.status(200).json('Not found');
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json(error);
+    } else {
+      res.status(500).json('Something went wrong!');
+    }
+  }
+}
