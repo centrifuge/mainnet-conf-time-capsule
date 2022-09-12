477be7e2047c125ee36c15525777872f2f1ed0bf
JP Angelle <jp@centrifuge.io>
2022-09-12 12:33:33 -0500

    clean up

--- client/next.config.js
+++ client/next.config.js
@@ -2,18 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: config => {
-    // eslint-disable-next-line no-param-reassign
-    config.resolve.fallback = {
-      fs: false,
-      child_process: false,
-      net: false,
-      tls: false,
-      request: false,
-    };
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
